3bd81d50b1cc16306393654cad8f82538bd82b7d
Luke Usher <flukus@gmail.com>
2010-11-10 15:14:26 +1100

    moved extractor helpers to sub folder

---
+++ src/app/doCS.Web/Helpers/Implimentation/Extractor/ExtractorData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using doCS.Extractor;
+using doCS.Models;
+
+namespace doCS.Web.Helpers.Implimentation {
+	public class ExtractorData {
+		public ProjectData ProjectData { get; private set; }
+		public Project Project { get; private set; }
+
+		public ExtractorData(ProjectData projectData, Project project) {
+			ProjectData = projectData;
+			Project = project;
+		}
+
+	}
+}
